15997e6529486c81372491ea378269554923826c
Patrick Greco <patrick.greco@mail.mcgill.ca>
2025-05-04 16:49:52 -0400

    Finished the guide for the simple HTTP server. I wanna play around a little bit and add some more to it.

--- src/main/java/SimpleHTTPServer.java
+++ src/main/java/SimpleHTTPServer.java
@@ -1,22 +1,24 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class SimpleHTTPServer {
 
-    public static void main(String[] args) throws Exception {
-        final ServerSocket server = new ServerSocket(8080);
+    public static void main(String args[]) throws IOException {
+
+        ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
-            while (!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: text/plain\r\n" +
+                        "Cache-Control: no-store\r\n" +
+                        "Connection: close\r\n\r\n" +
+                        today;
+                socket.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
             }
         }
     }
