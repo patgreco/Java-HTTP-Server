de44938fb8da81b7170b487b37bafae81ad60cc5
Patrick Greco <patrick.greco@mail.mcgill.ca>
2025-05-04 20:05:27 -0400

    Added multithreading for the HTTP server to handle clients concurrently.

--- src/main/java/SimpleHTTPServer.java
+++ src/main/java/SimpleHTTPServer.java
@@ -1,26 +1,66 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class SimpleHTTPServer {
+    public static void main(String args[]) {
+            try {
+                ServerSocket server = new ServerSocket(8080);
+                System.out.println("Listening for connection on port 8080 ....");
 
-    public static void main(String args[]) throws IOException {
+                while (true) {
+                    Socket socket = server.accept();
 
-        ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080 ....");
-        while (true) {
-            try (Socket socket = server.accept()) {
+                    Thread workerThread = new Thread(new WorkerThread(socket));
+                    workerThread.start();
+                }
+            }
+            catch (IOException e) {
+                System.err.println("Main thread exception!");
+                e.printStackTrace();
+            }
+    }
+
+    private static class WorkerThread implements Runnable {
+        Socket toClientSocket;
+
+        public WorkerThread (Socket toClientSocket) {
+            this.toClientSocket = toClientSocket;
+        }
+        public void run() {
+            System.out.println("Worker thread handling HTTP request");
+
+            try {
                 Date today = new Date();
                 String httpResponse = "HTTP/1.1 200 OK\r\n" +
                         "Content-Type: text/plain\r\n" +
                         "Cache-Control: no-store\r\n" +
-                        "Connection: close\r\n\r\n" +
+                        "Connection: close\r\n" +
+                        "Content-Length: " + today.toString().getBytes(StandardCharsets.UTF_8).length + "\r\n" +
+                        "\r\n" +
                         today;
-                socket.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
+                OutputStream out = toClientSocket.getOutputStream();
+                out.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+                out.flush();
+
+                System.out.println("Thread sleeping...");
+                try {
+                    Thread.sleep(10000);  // Sleep for 10 seconds to test concurrency handling
+                } catch (InterruptedException e) {
+                    System.err.println("Thread sleep exception!");
+                    e.printStackTrace();
+                }
+                System.out.println("Thread done sleeping.");
+
+                toClientSocket.close();
+            }
+            catch (IOException e) {
+                System.err.println("Worker thread exception!");
+                e.printStackTrace();
             }
         }
     }
-
 }
